37f50fdafd0bec7e8b4e0e84b2da3daee5e1cd9d
Mehmet Akif VARDAR <75534401+devmehmetakifv@users.noreply.github.com>
2025-12-19 11:27:35 +0300

    Fix Google CPU tracking and improve CLI handling

--- dowts.go
+++ dowts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
@@ -8,13 +9,62 @@ import (
 )
 
 func main() {
-	in1, _ := strconv.Atoi(os.Args[1])
-	//in2, _ := strconv.Atoi(os.Args[2])
-	//in3, _ := strconv.Atoi(os.Args[3])
-	//in4, _ := strconv.Atoi(os.Args[4])
-	//in5, _ := strconv.ParseFloat(os.Args[5], 64)
-	//in6, _ := strconv.ParseFloat(os.Args[6], 64)
+	args := os.Args[1:]
+	if len(args) == 1 {
+		scenario, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid scenario id: %v\n", err)
+			os.Exit(1)
+		}
+		UsageGenerator.Auto(scenario)
+		return
+	}
 
-	//UsageGenerator.Start(in1, in2, in3, in4, in5, in6)
-	UsageGenerator.Auto(in1)
+	if len(args) == 6 {
+		in1, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid input1: %v\n", err)
+			os.Exit(1)
+		}
+		in2, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid input2: %v\n", err)
+			os.Exit(1)
+		}
+		in3, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid input3: %v\n", err)
+			os.Exit(1)
+		}
+		in4, err := strconv.Atoi(args[3])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid input4: %v\n", err)
+			os.Exit(1)
+		}
+		in5, err := strconv.ParseFloat(args[4], 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid input5: %v\n", err)
+			os.Exit(1)
+		}
+		in6, err := strconv.ParseFloat(args[5], 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid input6: %v\n", err)
+			os.Exit(1)
+		}
+
+		UsageGenerator.Start(in1, in2, in3, in4, in5, in6)
+		return
+	}
+
+	printUsage()
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, "DoWTS (Dec 2025 pricing)\n")
+	fmt.Fprintf(os.Stderr, "\nAuto mode (recommended):\n")
+	fmt.Fprintf(os.Stderr, "  go run dowts.go <scenario_id>\n")
+	fmt.Fprintf(os.Stderr, "\nManual mode (advanced):\n")
+	fmt.Fprintf(os.Stderr, "  go run dowts.go <input1> <input2> <input3> <input4> <input5> <input6>\n")
+	fmt.Fprintf(os.Stderr, "\nSee README for parameter details.\n")
+	os.Exit(1)
 }
